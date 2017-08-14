3bb82d948ab4660bc93e4798b8c1ef9e2458fcdf
AndroPlus <hosigumayuugi@gmail.com>
2017-08-14 13:00:01 +0900

    Add hardware info props
    Change-Id: Ia05ed96f05a2f7fe9251a5fb37a434d6b84ce765

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,3 +26,6 @@ BOARD_KERNEL_CMDLINE += androidboot.hardware=maple
 
 # Enable real time lockscreen charging current values
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
+# Props
+TARGET_SYSTEM_PROP += device/sony/maple/system.prop
